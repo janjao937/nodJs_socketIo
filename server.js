8793eccd91ef118e13a149ec2b89fe6e80c2a1c8
janjao937 <janjao937@gmail.com>
2023-11-13 21:24:47 +0700

    ChatFunction

--- server.js
+++ server.js
@@ -4,40 +4,42 @@ const http = require("http");
 const cors = require("cors");
 const {Server} = require("socket.io");
 const PORT = 3001;
-
+const Chat = require("./src/chatSocket");
 
 app.use(cors());
 
 
 const server = http.createServer(app);
-const io = new Server(server,{
-    cors:{
-        origin:"http://localhost:3000", //react path
-        methods:["GET","POST"]//req
-    }
-});
+// const io = new Server(server,{
+//     cors:{
+//         origin:"http://localhost:3000", //react path
+//         methods:["GET","POST"]//req
+//     }
+// });
 
-//listen on the connection event for incoming sockets
-io.on("connection",(e)=>{
-    console.log("User connected",e.id);//user id
+// //listen on the connection event for incoming sockets
+// io.on("connection",(e)=>{
+//     console.log("User connected",e.id);//user id
 
-    e.on("join_room",(roomId)=>{
-        e.join(roomId);
-        console.log("USER WITH ID:",e.id,"Join Room",roomId);
-    });
-
-
-    //send_message event name like front
-    e.on("send_message",(data)=>{
-        e.to(data.room).emit("recive_message",data);
-    });
+//     e.on("join_room",(roomId)=>{
+//         e.join(roomId);
+//         console.log("USER WITH ID:",e.id,"Join Room",roomId);
+//     });
 
 
-    e.on("disconnect",()=>{
-        console.log("User disconnect",e.id);
-    });
+//     //send_message event name like front
+//     e.on("send_message",(data)=>{
+//         e.to(data.room).emit("recive_message",data);
+//     });
 
-});
+
+//     e.on("disconnect",()=>{
+//         console.log("User disconnect",e.id);
+//     });
+
+// });
+
+Chat(server);//chat function 
 
 
 server.listen(PORT,()=>{
---
+++ src/chatSocket.js
@@ -0,0 +1,49 @@
+const {Server} = require("socket.io");
+const http = require("http");
+const cors = require("cors");
+
+// const chat = {};
+
+
+
+const setCorsSocketIo = (server)=>{
+    return new Server(server,{
+        cors:{
+                origin:"http://localhost:3000", //react path
+                methods:["GET","POST"]//req
+            }
+        });
+}
+
+const chatConnection = (io)=>{
+    io.on("connection",(e)=>{
+        console.log("User connected",e.id);//user id
+    
+        e.on("join_room",(roomId)=>{
+            e.join(roomId);
+            console.log("USER WITH ID:",e.id,"Join Room",roomId);
+        });
+    
+    
+        //send_message event name like front
+        e.on("send_message",(data)=>{
+            e.to(data.room).emit("recive_message",data);
+        });
+    
+    
+        e.on("disconnect",()=>{
+            console.log("User disconnect",e.id);
+        });
+    
+    });
+}
+
+const Chat = (server) =>{
+   return chatConnection( setCorsSocketIo(server))
+}
+
+
+// chat.setCorsSocketIo = setCorsSocketIo;
+// chat.chatConnection = chatConnection;
+
+module.exports = Chat;
